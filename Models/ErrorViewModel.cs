f35941f89e3c007b0cf7d9c65a306b4a10630bb7
kashish kumari <164066541+Kashish-ratnani@users.noreply.github.com>
2024-03-20 13:06:23 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KashishKumari_SP22_BSCS_0091_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
